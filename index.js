9657da0818ab84cc796a386d116e1cd2b4f6b307
Jair Partida <jairpartida1@gmail.com>
2022-01-18 21:57:33 -0700

    adding ts

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+// Object Types Challenge
+// Based on what we discussed we need to make up our Property Objects and array,
+// can you create that array, making sure to assign the correct Types?
+exports.__esModule = true;
+var utils_1 = require("./utils");
+var isOpen;
+// Reviews
+var reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+];
+// User
+var you = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+};
+//Properties
+var properties = [
+    {}
+];
+// Functions
+(0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+(0, utils_1.populateUser)(you.isReturning, you.firstName);
